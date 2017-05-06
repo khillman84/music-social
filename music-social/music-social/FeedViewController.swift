d9f054d6d5d8c4f7c9290fb4c891dd32e4be8c9f
Kyle Hillman <khillman84@gmail.com>
2017-05-06 09:58:26 -0700

    got captions and likes displaying in app

--- music-social/music-social/FeedViewController.swift
+++ music-social/music-social/FeedViewController.swift
@@ -61,9 +61,13 @@ extension FeedViewController {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("caption: \(post.caption)")
         
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-    }
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
+     }
     
 }
